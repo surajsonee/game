af9c80d18745d70de642e29e433f3cda7fc26bcc
Perfect <suraj1.weavebytes@gmail.com>
2016-05-09 14:52:59 +0530

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -86,14 +86,10 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'finalapp',
-        'USER': 'root',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '',
-    },
+       'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
